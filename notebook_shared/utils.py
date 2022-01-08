4cd75e0d87d9a090088e4f138bf3814b68361e2a
Leonard <35725355+l8518@users.noreply.github.com>
2022-01-08 17:01:19 +0100

    Add folder based plotting

--- notebook_shared/utils.py
+++ notebook_shared/utils.py
@@ -13,16 +13,23 @@ def get_dataset_path(DATASET_NAME, FSIZE, DATA_FOLDER='data'):
     full_filename = f"{DATASET_NAME}{SIZES[FSIZE]}.parquet"
     return os.path.join(DATA_FOLDER, full_filename)
 
-def plot(name, **kwdata):
-    if not os.path.isdir(PLOT_FOLDER):
-       os.makedirs(PLOT_FOLDER)
-    plt.savefig(os.path.join(PLOT_FOLDER, f"{name}.pdf"))
+def plot(name_or_name_components, **kwdata):
+
+    if (type(name_or_name_components) is list):
+        name = name_or_name_components[-1]
+        folder = os.path.join(PLOT_FOLDER, *(name_or_name_components[:-1]))
+    else:
+        name = name_or_name_components
+        folder = PLOT_FOLDER
+    if not os.path.isdir(folder):
+       os.makedirs(folder)
+    plt.savefig(os.path.join(folder, f"{name}.pdf"))
     plt.show()
     plt.close()
     for key in kwdata:
         data = kwdata[key]
         str_resp = str(data)
-        with open(os.path.join(PLOT_FOLDER, f"{name}_{key}.txt"), 'w') as f:
+        with open(os.path.join(folder, f"{name}_{key}.txt"), 'w') as f:
             f.write(str_resp)
             print(key, "\n", str_resp)
 
