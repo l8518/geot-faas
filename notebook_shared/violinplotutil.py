76c646818c709e6a7cd7dc9af861320b49ab2cfa
Leonard <35725355+l8518@users.noreply.github.com>
2022-05-04 21:41:18 +0200

    fix violinplots

--- notebook_shared/violinplotutil.py
+++ notebook_shared/violinplotutil.py
@@ -23,6 +23,9 @@ class CustomViolinPlotter(_ViolinPlotter):
                  width, inner, split, dodge, orient, linewidth,
                  color, palette, saturation, customlineswidth = 4):
         
+        if (customlineswidth is None):
+                customlineswidth = 4
+
         self.customlineswidth = customlineswidth
         
         super().__init__(x, y, hue, data, order, hue_order,
@@ -38,11 +41,12 @@ class CustomViolinPlotter(_ViolinPlotter):
         # include code from super().draw_box_lines(ax, data, support, density, center)
         # to improve custom plotting
         q25, q50, q75 = np.percentile(data, [25, 50, 75])
-        whisker_lim = 1.5 * (q75 - q25)
+        iqr = (q75 - q25)
+        whisker_lim = 1.5 * iqr
         h1 = np.min(data[data >= (q25 - whisker_lim)])
         h2 = np.max(data[data <= (q75 + whisker_lim)])
 
-        # Draw a boxplot using lines and a point
+        # Draw a boxplot using lines
         if self.orient == "v":
             ax.plot([center, center], [h1, h2],
                     linewidth=self.linewidth,
@@ -50,11 +54,11 @@ class CustomViolinPlotter(_ViolinPlotter):
             ax.plot([center, center], [q25, q75],
                     linewidth=self.linewidth * 3,
                     color=self.gray, alpha=0.8, solid_capstyle='butt')
-            ax.scatter(center, q50,
-                       zorder=3,
-                       color="white",
-                       edgecolor=self.gray,
-                       s=np.square(self.linewidth * 3))
+            ax.plot([center - 0.1, center + 0.1], [q50, q50],
+                zorder=3,
+                linewidth=self.linewidth,
+                alpha=0.8,
+                color='red')   
             # Improved Whiskers:
             # min
             ax.plot([center - 0.1, center + 0.1], [h2, h2],
@@ -72,19 +76,19 @@ class CustomViolinPlotter(_ViolinPlotter):
             ax.plot([q25, q75], [center, center],
                     linewidth=self.linewidth * 3,
                     color=self.gray)
-            ax.scatter(q50, center,
-                       zorder=3,
-                       color="red",
-                       edgecolor=self.gray,
-                       s=np.square(self.linewidth * 3))       
+            ax.plot([center - 0.1, center + 0.1], [q50, q50],
+                zorder=3,
+                linewidth=self.linewidth,
+                alpha=0.8,
+                color='red')     
         
         
         # Plot mean line into violinplot
         mean = np.mean(data)
         ax.plot([center - 0.1, center + 0.1], [mean, mean],
                 linewidth=2,
-                alpha=0.5,
-                color='red', zorder = 5)
+                alpha=0.8,
+                color='orangered', zorder = 5)
         
         self.linewidth = defaultlinewidth
 
@@ -108,11 +112,4 @@ def violinplot(
         ax = plt.gca()
 
     plotter.plot(ax)
-    return ax
-
-# Custom Violin Plots
-def vioplot(pdf, axes, col, **kwargs):
-    tmp = pd.DataFrame(pdf.to_dict()) #TODO: Improve?
-    ax = violinplot(**kwargs, x="region", y=col, data=tmp, ax=axes,
-                    inner='box', linewidth=0, rot=45)
     return ax
